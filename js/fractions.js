2874f5816b2bf700cac4151716dc8a20d95baf06
saskcow <callum@tarttelin.co.uk>
2014-04-22 19:02:11 +0100

    segment select W.I.P (clicking segments temporarily broken)

--- js/fractions.js
+++ js/fractions.js
@@ -51,8 +51,20 @@ var Segment = function(angle, id) {
 
     this.isClicked = function(x, y) {
         var hypotenusifier = (x - 300) * (x - 300) + (y - 400) * (y - 400);
-        return Math.sqrt(hypotenusifier) < 100;
+        if (Math.sqrt(hypotenusifier) > 100) return false;
+        var LR, UD, pos = undefined;
+        if (x > 300) {LR = 10} else{LR = 0};
+        if (y > 400) {UD = 5} else{UD = 0};
+        if (LR + UD = 15) {pos = 1};
+        if (LR + UD = 10) {pos = 2};
+        if (LR + UD = 0) {pos = 3};
+        if (LR + UD = 5) {pos = 4};
 
+        var sectorifier = Math.atan((x - 300) / (y - 400));
+        var angle = sectorifier + (90 * (pos - 1));
+        var segment_select = (360 / segments);
+        return this.id == Math.floor(angle / segment_select);
+        
     };
 
     this.selected = function() {
