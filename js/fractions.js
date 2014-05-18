34fc7a757069d2f16e2a5ecc4e8685482f13a65c
pyruby <chris@pyruby.co.uk>
2014-05-18 12:23:25 +0100

    - Make it pretty - Determine greatest common denominator to simplify the fraction - Mark it.

--- js/fractions.js
+++ js/fractions.js
@@ -9,7 +9,18 @@ var segments = validValues[Math.floor(Math.random() * validValues.length)];
 
 var fraction = Math.floor(Math.random() * (segments - 1)) + 1;
 
-$("#fraction").text(fraction + "/" + segments);
+function reduce(numerator,denominator) {
+    var gcd = function gcd(a, b) {
+        return b ? gcd(b, a % b) : a;
+    };
+    gcd = gcd(numerator, denominator);
+    return [numerator / gcd, denominator / gcd];
+}
+
+var simplifiedFraction = reduce(fraction, segments);
+
+$("#fraction").text(simplifiedFraction[0] + "/" + simplifiedFraction[1]);
+var centre = {x: 100, y: 100, radius: 80};
 
 function rad(num) {
     return (num - 90) / 360 * (2 * Math.PI);
@@ -38,29 +49,29 @@ var Segment = function(angle, id) {
     this.id = id;
     this.isSelected = false;
 
-    this.draw = function(context) {
-        context.fillStyle = "#FFFFFF";
+    this.draw = function(context, fillColour) {
+        context.fillStyle = fillColour ? fillColour : "#FFFFFF";
         context.beginPath();
-        context.moveTo(300, 400);
-        context.lineTo(300 + (100 * Math.cos(rad(this.angle * this.id))), 400 + (100 * Math.sin(rad(this.angle * this.id))));
-        context.arc(300, 400, 100, rad(this.angle * this.id), rad(this.angle * (this.id + 1)));
-        context.lineTo(300, 400);
+        context.moveTo(centre.x, centre.y);
+        context.lineTo(centre.x + (centre.radius * Math.cos(rad(this.angle * this.id))), centre.y + (centre.radius * Math.sin(rad(this.angle * this.id))));
+        context.arc(centre.x, centre.y, centre.radius, rad(this.angle * this.id), rad(this.angle * (this.id + 1)));
+        context.lineTo(centre.x, centre.y);
         context.fill();
         context.stroke();
     };
 
     this.isClicked = function(x, y) {
-        var hypotenusifier = (x - 300) * (x - 300) + (y - 400) * (y - 400);
-        if (Math.sqrt(hypotenusifier) > 100) return false;
+        var hypotenusifier = (x - centre.x) * (x - centre.x) + (y - centre.y) * (y - centre.y);
+        if (Math.sqrt(hypotenusifier) > centre.radius) return false;
         var lr, ud, pos = undefined;
-        if (x > 300) {lr = 10;} else {lr = 0;}
-        if (y > 400) {ud = 5;} else {ud = 0;}
+        if (x > centre.x) {lr = 10;} else {lr = 0;}
+        if (y > centre.y) {ud = 5;} else {ud = 0;}
         if (lr + ud === 15) {pos = 2;}
         if (lr + ud === 10) {pos = 1;}
         if (lr + ud === 0) {pos = 4;}
         if (lr + ud === 5) {pos = 3;}
 
-        var sectorifier = (360 / (2 * Math.PI) ) * Math.atan(Math.abs(x - 300) / Math.abs(y - 400));
+        var sectorifier = (360 / (2 * Math.PI) ) * Math.atan(Math.abs(x - centre.x) / Math.abs(y - centre.y));
         if (pos === 2 || pos === 4) {
         	sectorifier = 90 - sectorifier;
         }
@@ -70,21 +81,27 @@ var Segment = function(angle, id) {
         console.log("pos: " + pos);
         console.log("sectorifier: " + sectorifier);
         return this.id == Math.floor(angle / anglometer);
-
     };
 
     this.selected = function() {
-        this.isSelected = true;
-        context.fillStyle = "#FF00FF";
-        context.beginPath();
-        context.moveTo(300, 400);
-        context.lineTo(300 + (100 * Math.cos(rad(this.angle * this.id))), 400 + (100 * Math.sin(rad(this.angle * this.id))));
-        context.arc(300, 400, 100, rad(this.angle * this.id), rad(this.angle * (this.id + 1)));
-        context.lineTo(300, 400);
-        context.fill();
-        context.stroke();
+        if (this.isSelected) {
+            this.isSelected = false;
+            this.draw(context, "#FFFFFF");
+        } else {
+            this.isSelected = true;
+            this.draw(context, "#FF00FF");
+        }
     };
 };
+
+function checkAnswer() {
+    var actualSelected = segmentList.filter(function(segment) { return segment.isSelected}).length;
+    if (actualSelected === fraction) {
+        $('#result').html("YES!!!");
+    } else {
+        $('#result').html("NO :-(");
+    }
+}
 
 for(segmentNum = 0; segmentNum < segments; segmentNum++){
     var segment = new Segment(360 / segments, segmentNum);
